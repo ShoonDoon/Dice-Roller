6a079bf95e7e5232937c2130ce9bb746a50d13e8
Maximiliano <lmax23072002@gmail.com>
2023-12-08 10:33:12 +0200

    adopted good coding practices

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,23 +11,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find the button by its identifier and assign a click event handler
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            rollButton.setOnClickListener {
-                rollDice()
-            }
-    }
-}
+            rollDice() // Call the rollDice() function when you click on the button
 
+        }
+    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
-    class Dice(private val numSides: Int) {
 
-    fun roll(): Int {
-        return (1..numSides).random()}
+
+    // A class that represents a cube with the specified number of faces
+    class Dice(private val numSides: Int) {
+        // A function that rolls a dice and returns the result
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
     }
 }
